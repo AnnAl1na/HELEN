6d1adf3497778e8aa681e79e0c933f58d6296f39
AnnAl1na <anntomyaleena@gmail.com>
2025-01-25 07:16:31 +0530

    Add files via upload

---
+++ query_data.py
@@ -0,0 +1,63 @@
+import argparse
+from langchain_chroma import Chroma
+from langchain.prompts import ChatPromptTemplate
+from langchain_community.llms.ollama import Ollama
+from langchain_groq import ChatGroq
+
+from langchain_community.embeddings.fastembed import FastEmbedEmbeddings
+from get_embedding_function import get_embedding_function
+
+CHROMA_PATH = "chroma"
+
+PROMPT_TEMPLATE = """
+Answer the question based only on the following context:
+
+{context}
+
+---
+
+Answer the question based on the above context: {question}
+"""
+
+
+def main():
+    # Create CLI.
+    parser = argparse.ArgumentParser()
+    parser.add_argument("query_text", type=str, help="The query text.")
+    args = parser.parse_args()
+    query_text = args.query_text
+    query_rag(query_text)
+
+
+def query_rag(query_text: str):
+    # Prepare the DB.
+    #embedding_function = get_embedding_function()
+    db = Chroma(persist_directory=CHROMA_PATH, embedding_function=FastEmbedEmbeddings())
+
+    # Search the DB.
+    results = db.similarity_search_with_score(query_text, k=5)
+
+    context_text = "\n\n---\n\n".join([doc.page_content for doc, _score in results])
+    prompt_template = ChatPromptTemplate.from_template(PROMPT_TEMPLATE)
+    prompt = prompt_template.format(context=context_text, question=query_text)
+    #print(prompt)
+
+    #model = Ollama(model="mistral")
+    #response_text = model.invoke(prompt)
+    llm = ChatGroq(
+            model="llama-3.1-8b-instant",
+            temperature=0.5,
+            api_key = "....."
+        )
+    response_text = llm.invoke(prompt).content
+    #print(response_text)
+
+    sources = [doc.metadata.get("id", None) for doc, _score in results]
+    formatted_response = f"Response: {response_text}"
+    #\nSources: {sources}"
+    print(formatted_response)
+    return response_text
+
+
+if __name__ == "__main__":
+    main()
